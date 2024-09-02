68073088f48316732baff2130abce79b2864243b
MariaDumitru25 <maria.dumitru@student.uva.nl>
2024-09-02 13:24:34 +0200

    This is a message describing what I did

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,8 @@
+import my_file
+import my_name
+import name
+
+def team():
+    print(f"This is Team Group_Name(Optional). We are:",my_file.return_name(),",",my_name.return_name(),",",name.my_name())
+
+team()
