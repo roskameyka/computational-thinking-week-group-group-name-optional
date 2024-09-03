7cdbfb333d46d6d4947d2fc28e7911cb46066bce
brendanUva <brendan.corcoran@student.uva.nl>
2024-09-03 10:39:52 +0200

    finished station 1 and commented

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,4 +1,26 @@
+
+# fibonacci number function using recursion, found from https://www.geeksforgeeks.org/python-program-to-print-the-fibonacci-sequence/
+def fibonacci(n):
+
+    # if input is < 0 error is thrown
+    if n < 0:
+        raise Exception("Inputs < 0 do not work with Fibonacci")
+
+    # if input is 0, 0 is returned
+    elif n == 0:
+        return 0
+
+    # if input is 1 or 2, 1 is returned
+    elif n == 1 or n == 2:
+        return 1
+
+    # in all other cases, recursion is used to find number
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
 def solution_station_1(observation):
 
+    #using fibonacci recursion function to get number
+    outputFibonacciNumber = fibonacci(observation)
 
-    return 
+    return outputFibonacciNumber
