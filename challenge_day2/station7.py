448641fe4c4041e4145bec19c04a769017cb918a
roskameyka <alexandra.roskam@student.uva.nl>
2024-09-03 14:36:31 +0200

    changes

--- challenge_day2/station7.py
+++ challenge_day2/station7.py
@@ -1,6 +1,4 @@
 def solution_station_7(x):
-<<<<<<< HEAD
-    
     a=3.0
     b=-1.0
     c=4.0
@@ -9,11 +7,3 @@ def solution_station_7(x):
 
     answer = eval(x)
     return answer
-=======
-    a=3
-    b=-1
-    c=4
-    d=7
-    e=0.5
-    return eval(x)
->>>>>>> refs/remotes/origin/main
