d855a149995c2e9a2907afc2ed4ce45e07627162
brendanUva <brendan.corcoran@student.uva.nl>
2024-09-03 10:21:42 +0200

    finished station 2 function

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,3 +1,19 @@
+import datetime
+import calendar
+from translate import Translator
+
+translator= Translator(to_lang="ja")
+
 def solution_station_2(observation):
 
-    return
+    format = '%Y-%m-%d'
+
+    dateObject = datetime.datetime.strptime(observation, format)
+
+    dayOfWeek = calendar.day_name[dateObject.weekday()] 
+
+    translation = translator.translate(dayOfWeek)
+
+    print(translation)
+
+    return translation
